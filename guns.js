9220c4c4896d0997aa5f5e94cea1f0a4b9e95c42
sean111111111vrotato <santillanrosanna89@gmail.com>
2026-02-11 09:05:22 +0800

    Add VALORANT_GUNS data structure

---
+++ guns.js
@@ -0,0 +1,19 @@
+const VALORANT_GUNS = {
+  "Classic":    {mag:12, reserve:36, damage:26, fireRate:6},
+  "Shorty":     {mag:2, reserve:12, damage:12, fireRate:2.5},
+  "Frenzy":     {mag:13, reserve:39, damage:16, fireRate:10},
+  "Ghost":      {mag:15, reserve:45, damage:30, fireRate:6.75},
+  "Sheriff":    {mag:6, reserve:24, damage:55, fireRate:4},
+  "Stinger":    {mag:20, reserve:120, damage:15, fireRate:18},
+  "Spectre":    {mag:30, reserve:90, damage:26, fireRate:13},
+  "Bulldog":    {mag:24, reserve:60, damage:35, fireRate:6.5},
+  "Guardian":   {mag:12, reserve:36, damage:54, fireRate:6},
+  "Phantom":    {mag:30, reserve:90, damage:35, fireRate:11},
+  "Vandal":     {mag:25, reserve:75, damage:40, fireRate:9.25},
+  "Marshal":    {mag:5, reserve:15, damage:202, fireRate:1},
+  "Operator":   {mag:5, reserve:15, damage:150, fireRate:0.75},
+  "Bucky":      {mag:5, reserve:15, damage:34, fireRate:1},
+  "Judge":      {mag:7, reserve:28, damage:32, fireRate:3.5},
+  "Ares":       {mag:50, reserve:150, damage:30, fireRate:12},
+  "Odin":       {mag:100, reserve:200, damage:36, fireRate:12}
+};
